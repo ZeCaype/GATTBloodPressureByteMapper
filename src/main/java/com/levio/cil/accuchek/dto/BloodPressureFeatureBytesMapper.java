d7e8cf749dd99763b3ff3979b14b45db70f7538e
ZeCaype <jowpacabra@gmail.com>
2019-06-06 14:04:43 -0400

    Added BloodPressureFeatureDto and mappers

---
+++ src/main/java/com/levio/cil/accuchek/dto/BloodPressureFeatureBytesMapper.java
@@ -0,0 +1,53 @@
+package com.levio.cil.accuchek.dto;
+
+import com.levio.cil.accuchek.rest.utils.ByteUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BloodPressureFeatureBytesMapper {
+
+
+  public BloodPressureFeatureBytesMapper() {
+
+  }
+
+  public BloodPressureFeatureDto mapArrayOfBytesToReadableData(int[] dataDto) {
+
+    BloodPressureFeatureDto bloodFeature = new BloodPressureFeatureDto();
+
+    String rawBloodPressureFeatureByte1 = ByteUtils.intToBinaryString(dataDto, 0);
+    String rawBloodPressureFeatureByte2 = ByteUtils.intToBinaryString(dataDto, 1);
+    rawBloodPressureFeatureByte1 = new StringBuilder(rawBloodPressureFeatureByte1).reverse().toString();
+    rawBloodPressureFeatureByte2 = new StringBuilder(rawBloodPressureFeatureByte2).reverse().toString();
+    int bitCount = 0;
+
+    for (char bit : rawBloodPressureFeatureByte1.toCharArray()) {
+      switch (bitCount) {
+        case 0:
+          bloodFeature.setBodyMovementDetectionSupported(ByteUtils.binaryCharToBoolean(bit));
+          break;
+        case 1:
+          bloodFeature.setCuffFitDetectionSupported(ByteUtils.binaryCharToBoolean(bit));
+          break;
+        case 2:
+          bloodFeature.setIrregularPulseDetectionSupported(ByteUtils.binaryCharToBoolean(bit));
+          break;
+        case 3:
+          bloodFeature.setPulseRateRangeDetectionSupported(ByteUtils.binaryCharToBoolean(bit));
+          break;
+        case 4:
+          bloodFeature.setMeasurementPositionDetectionSupported(ByteUtils.binaryCharToBoolean(bit));
+          break;
+        case 5:
+          bloodFeature.setMultipleBondSupported(ByteUtils.binaryCharToBoolean(bit));
+          break;
+        default:
+          break;
+      }
+      bitCount++;
+    }
+    bitCount = 0;
+
+    return bloodFeature;
+  }
+}
--- src/main/java/com/levio/cil/accuchek/rest/controllers/BloodPressureMetricsController.java
+++ src/main/java/com/levio/cil/accuchek/rest/controllers/BloodPressureMetricsController.java
@@ -5,19 +5,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.levio.cil.accuchek.dto.BloodPressureFeatureBytesMapper;
 import com.levio.cil.accuchek.dto.BloodPressureFeatureDto;
 
 @RestController
 @RequestMapping(value = "/bloodpressure")
 public class BloodPressureMetricsController {
+  
+  private BloodPressureFeatureBytesMapper bloodPressureFeatureBytesMapper;
 
   @Autowired
-  public BloodPressureMetricsController() {
-
+  public BloodPressureMetricsController(BloodPressureFeatureBytesMapper bloodPressureFeatureBytesMapper) {
+    this.bloodPressureFeatureBytesMapper = bloodPressureFeatureBytesMapper;
   }
 
   @PostMapping (value = "/feature")
   public BloodPressureFeatureDto mapX(@RequestBody int[] dataDto) {
-    return null;
+    return bloodPressureFeatureBytesMapper.mapArrayOfBytesToReadableData(dataDto);
   }
 }
